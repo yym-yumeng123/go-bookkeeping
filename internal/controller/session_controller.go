47d013578778e0ca3ab40d1ca916409469671943
yym-yumeng123 <18026493873@163.com>
2023-12-01 16:04:31 +0800

    创建 session controller

---
+++ internal/controller/session_controller.go
@@ -0,0 +1,30 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type SessionController struct{}
+
+func (s SessionController) Create(c *gin.Context) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s SessionController) Destroy(c *gin.Context) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s SessionController) Update(c *gin.Context) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s SessionController) Get(c *gin.Context) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s SessionController) GetPaged(c *gin.Context) {
+	//TODO implement me
+	panic("implement me")
+}

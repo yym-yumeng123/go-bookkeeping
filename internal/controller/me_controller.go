0793c8e2a3ccbf7611f31a1eec76078d7322966f
yym-yumeng123 <18026493873@163.com>
2023-12-01 18:26:02 +0800

    jwt & 获取数据并Scan扫描

--- internal/controller/me_controller.go
+++ internal/controller/me_controller.go
@@ -8,8 +8,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"net/http"
-	"strconv"
 )
+
+type ResponseData struct {
+	ID    int
+	Email string
+}
 
 type MeController struct{}
 
@@ -37,35 +41,31 @@ func (m *MeController) Update(c *gin.Context) {
 
 func (m *MeController) Get(c *gin.Context) {
 	user := model.User{}
+	response := ResponseData{}
 	// 客户端发送请求携带 http header Authorization = Bearer <jwt>
 	auth := c.GetHeader("Authorization")
 	jwtString := auth[7:]
-	// 解析 jwtString 获取 user_id
-	t, err := utils.Parse(jwtString)
-	if err != nil {
-		c.String(http.StatusUnauthorized, "invalid jwt")
+
+	if auth == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "token字符串为空"})
 	}
-	if claims, ok := t.Claims.(jwt.MapClaims); ok {
-		if userId, ok := claims["user_id"].(string); !ok {
-			c.String(http.StatusUnauthorized, "invalid jwt")
-			return
-		} else {
-			if userIdInt, err := strconv.Atoi(userId); err != nil {
-				c.String(http.StatusUnauthorized, "invalid jwt")
-			} else {
-				// 通过 user_id 获取 user
-				tx := database.DB.First(&user, "id = ?", userIdInt)
-				if tx.Error != nil {
-					c.JSON(http.StatusOK, gin.H{
-						"success": true,
-						"data":    user,
-					})
-				}
-			}
+
+	// 解析 jwtString 获取 user_id
+	token, err := utils.Parse(jwtString)
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		// 通过 user_id 获取 user
+		tx := database.DB.Model(&model.User{}).First(&user, "id = ?", claims["user_id"]).Scan(&response)
+		if tx.Error == nil {
+			c.JSON(http.StatusOK, gin.H{
+				"success": true,
+				"data":    response,
+			})
 		}
 	} else {
 		fmt.Println(err)
 	}
+
 }
 
 func (m *MeController) GetPaged(c *gin.Context) {
